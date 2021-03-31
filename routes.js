e1da8f27a255508d5b77f52b8619c15442864c19
Steve <catholickiwi2016@gmail.com>
2021-03-31 13:45:00 +1300

    Add in the Routes page thingee

---
+++ routes.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const fs = require('fs')
+const router = express.Router()
+ 
+ //This is rendering buyArtPage.hbs
+ router.get('/buyArtPage', (req,res) => {
+  fs.readFile('./data.json', 'utf-8', (err, data) => {
+      if (err) return res.status(500)
+      const parsedData = JSON.parse(data)
+      console.log(parsedData)
+  res.render('buyArtPage' , parsedData)
+})
+})
+
+//This is rendering uploadMoreArtPage.hbs
+router.get('/uploadArt', (req,res) => {
+  res.render('uploadMoreArtPage')
+})
+
+router.post('/uploadMoreArtPage', (req, res) => {
+ // read the current data
+fs.readFile('./data.json', 'utf-8', (err, existingData) => {
+      if (err) return res.status(500)
+      const parsedData = JSON.parse(existingData)
+
+      //new data coming in
+      const newData = req.body
+
+      // write the new data (req.body)
+      let newArtObj =  {
+        id: parsedData.artwork.length+1,
+        name: newData.name,
+        artist: newData.artist,
+        image: "/images/fantail1.jpg",
+        price: newData.price
+      }
+
+      parsedData.artwork.push(newArtObj)
+
+      // write file with the new data
+      fs.writeFile('./data.json', JSON.stringify(parsedData, null, 2), (err) => {
+        if (err) return res.status(500)
+        else res.redirect('/buyArtPage')
+      })
+    })
+})
+
+ //This is rendering contactPage.hbs
+router.get('/contact', (req,res) => {
+res.render('contactPage')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const hbs = require('express-handlebars')
 const fs = require('fs')
 const bodyParser = require("body-parser");
 
+const artRoutes = require('./routes')
 const server = express()
 
 // Middlewares
 server.use(express.static('public'))
 server.use(express.urlencoded({ extended: false }))
+
+server.use('/', artRoutes)
 
 // Handlebars 
 server.engine('hbs', hbs({ extname: 'hbs' }))
@@ -18,63 +21,5 @@ server.set('view engine', 'hbs')
 server.get('/', (req,res) => {
     res.render('homePage' /*, parsedData*/)
  })
-
- //This is rendering buyArtPage.hbs
-server.get('/buyArtPage', (req,res) => {
-    fs.readFile('./data.json', 'utf-8', (err, data) => {
-        if (err) return res.status(500)
-        const parsedData = JSON.parse(data)
-        console.log(parsedData)
-    res.render('buyArtPage' , parsedData)
- })
-})
-
-  //This is rendering uploadMoreArtPage.hbs
-  server.get('/uploadArt', (req,res) => {
-    res.render('uploadMoreArtPage')
- })
-
-
-  //This is rendering contactPage.hbs
-server.get('/contact', (req,res) => {
-    res.render('contactPage')
- })
-
- 
- server.post('/uploadMoreArtPage', (req, res) => {
-   // read the current data
-  fs.readFile('./data.json', 'utf-8', (err, existingData) => {
-        if (err) return res.status(500)
-        const parsedData = JSON.parse(existingData)
-
-        //new data coming in
-        const newData = req.body
-
-        // write the new data (req.body)
-        let newArtObj =  {
-          id: parsedData.artwork.length+1,
-          name: newData.name,
-          artist: newData.artist,
-          image: "/images/fantail1.jpg",
-          price: newData.price
-        }
-
-        parsedData.artwork.push(newArtObj)
-
-
-        // write file with the new data
-        fs.writeFile('./data.json', JSON.stringify(parsedData, null, 2), (err) => {
-          if (err) return res.status(500)
-          else res.redirect('/buyArtPage')
-        })
-      })
- })
-
-
-  
-
-
-
-
 
 module.exports = server
